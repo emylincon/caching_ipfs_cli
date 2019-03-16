4c7af89e427ff3fbd1c8c6916f43a882598b6b1d
Emeka E Ugwuanyi <emylincon@gmail.com>
2019-03-16 18:56:41 +0000

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -5,7 +5,7 @@ apt update && apt upgrade -y
 clear
 
 echo 'setting up environment... '
-sleep 3
+sleep 2
 apt install sqlite3 -y
 
 apt-get install nano -y
@@ -21,6 +21,19 @@ apt-get install python3-psutil -y
 apt-get install python3-pyfiglet -y
 apt-get install python3-matplotlib -y
 
+clear
+echo 'Preparing database'
+sleep 2
+
+mv * ..
+python3 /home/mec/files_cache/refresh_db.py
+/etc/init.d/ssh start
+clear
+
+echo 'database done!'
+echo 'setting up ipfs'
+sleep 1.5
+
 wget https://dist.ipfs.io/go-ipfs/v0.4.18/go-ipfs_v0.4.18_linux-amd64.tar.gz
 
 tar xvfz go-ipfs_v0.4.18_linux-amd64.tar.gz
@@ -28,3 +41,6 @@ tar xvfz go-ipfs_v0.4.18_linux-amd64.tar.gz
 cd go-ipfs
 
 bash install.sh
+
+clear
+echo 'All done, Ready to use'
