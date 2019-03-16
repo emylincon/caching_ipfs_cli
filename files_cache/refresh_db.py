ca4f7496ea58f47f8ecfc2c70cb94a25f41e808e
Emeka E Ugwuanyi <emylincon@gmail.com>
2019-03-16 19:06:38 +0000

    Update refresh_db.py

--- files_cache/refresh_db.py
+++ files_cache/refresh_db.py
@@ -1,18 +1,43 @@
 import sqlite3
 import os
  
-con = sqlite3.connect('/home/mec/cache.db')
-cur = con.cursor()
  
-cur.execute("delete FROM CacheTable WHERE Host_ip in ('192.168.40.136', '10.0.0.10', '10.0.0.20', '10.0.0.30')")
-con.commit()
+def main():
+    try:
+        con = sqlite3.connect('/home/mec/cache.db')
+        cur = con.cursor()
+        cur.executescript('''DROP TABLE IF EXISTS CacheTable;
+            CREATE TABLE CacheTable(Ipfs_Hash varchar(50), Hash varchar(30), Path varchar(70), DateTime varchar(30), Host_ip varchar(16));
+            INSERT INTO CacheTable (Ipfs_Hash, Hash, Path, DateTime, Host_ip) VALUES('79IURHDF97WIEHUFN', '123a11', '/cache/data1.txt', '12:00', '192.168.1.1');
+            INSERT INTO CacheTable (Ipfs_Hash, Hash, Path, DateTime, Host_ip) VALUES('927EHD8IEHD29EIDH', '127b23', '/cache/data2.txt', '12:05', '192.168.1.2');''')
  
-cur.execute("select * from CacheTable")
-data = cur.fetchall()
+        con.commit()
  
-for row in data:
-    print(row)
+        cur.execute("SELECT * FROM CacheTable")
  
-con.close()
+        data = cur.fetchall()
+ 
+        for row in data:
+            print(row)
+ 
+    except sqlite3.Error as e:
+        if con:
+            con.rollback()
+            print('Error Encountered: {}'.format(e))
+ 
+    finally:
+        if con:
+            con.close()
+ 
+ 
+def clear_temp():
+    try:
+        os.system('rm /home/mec/temp/*')
+        os.system('rm /home/mec/cache/*')
+    except:
+        print('Database is refreshed')
 
-os.system('rm /home/mec/cache/*.html')
+
+if __name__ == "__main__":
+    main()
+    clear_temp()
