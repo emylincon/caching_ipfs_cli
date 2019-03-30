d62ea702aff165fbe0b3d585252e7740a3faf090
Emeka E Ugwuanyi <emylincon@gmail.com>
2019-03-30 18:29:35 +0000

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -35,7 +35,7 @@ wget https://dist.ipfs.io/go-ipfs/v0.4.18/go-ipfs_v0.4.18_linux-amd64.tar.gz
 
 tar xvfz go-ipfs_v0.4.18_linux-amd64.tar.gz
 
-bash go-ipfs/install.sh
+sudo bash go-ipfs/install.sh
 
 
 echo 'IPFS done..'
